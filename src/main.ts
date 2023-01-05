73553d02b60ba254f73734f56d0ef531940f158f
Alexander <orientstar96@gmail.com>
2023-01-05 12:28:27 +0500

    2.3 Меняет настройки контейнера

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import Application from './app/application.js';
 import ConfigService from './common/config/config.service.js';
 import LoggerService from './common/logger/logger.service.js';
 
-const appContainer = new Container();
-appContainer.bind(Component.Application).to(Application).inSingletonScope();
-appContainer.bind(Component.ConfigInterface).to(ConfigService).inSingletonScope();
-appContainer.bind(Component.LoggerInterface).to(LoggerService).inSingletonScope();
+const appContainer = new Container({ defaultScope: 'Singleton' });
+appContainer.bind(Component.Application).to(Application);
+appContainer.bind(Component.ConfigInterface).to(ConfigService);
+appContainer.bind(Component.LoggerInterface).to(LoggerService);
 
 const application = appContainer.get<Application>(Component.Application);
 application.init();
